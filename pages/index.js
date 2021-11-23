86783096af2299932f9b103e3a7711d5c02ab340
Farah <farah.zaqout@gmail.com>
2021-11-23 19:36:31 +0200

    change the hello world for deployment on preview only

--- pages/index.js
+++ pages/index.js
@@ -12,7 +12,7 @@ export default function Home() {
       </Head>
 
       <main className={styles.main}>
-        <h1>Hello World</h1>
+        <h1>Hello World We're only on Preview, bitches!!!</h1>
       </main>
 
       <footer className={styles.footer}>
